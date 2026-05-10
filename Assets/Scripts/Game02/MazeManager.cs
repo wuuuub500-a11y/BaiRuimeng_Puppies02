0d93c89dbfb2b798fa997262bc61c53b66b69b02
wuuuub500-a11y <wuuuub500@gmail.com>
2026-05-10 20:58:34 +0800

    510

---
+++ Assets/Scripts/Game02/MazeManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeManager : MonoBehaviour
+{
+    void Start()
+    {
+        RandomizeMaze();
+    }
+
+    void RandomizeMaze()
+    {
+        int childCount = transform.childCount;
+
+        if (childCount == 0) return;
+
+        int randomIndex = Random.Range(0, childCount);
+
+        for (int i = 0; i < childCount; i++)
+        {
+            GameObject child = transform.GetChild(i).gameObject;
+
+            if (i == randomIndex)
+            {
+                child.SetActive(true);
+            }
+            else
+            {
+                child.SetActive(false);
+            }
+        }
+
+        Debug.Log("当前加载的迷宫索引是: " + randomIndex);
+    }
+}
